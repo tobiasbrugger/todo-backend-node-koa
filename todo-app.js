820e12fb32d7a2fbaa20191562f5238944bb1e6a
Tobias Brugger <tobias.brugger@bfh.ch>
2024-09-23 09:10:09 +0200

    exercise step 1 finished

--- todo-app.js
+++ todo-app.js
@@ -1,60 +1,132 @@
-const router = require('koa-router')();
+const router = require("koa-router")();
 
-const Koa = require('koa');
-const bodyParser = require('koa-bodyparser');
-const cors = require('@koa/cors');
+const Koa = require("koa");
+const bodyParser = require("koa-bodyparser");
+const cors = require("@koa/cors");
 
 const app = new Koa();
 
 let todos = {
-  0: {'title': 'build an API', 'order': 1, 'completed': false},
-  1: {'title': '?????', 'order': 2, 'completed': false},
-  2: {'title': 'profit!', 'order': 3, 'completed': false}
+  0: {
+    title: "build an API",
+    order: 1,
+    completed: false,
+    tags: [],
+    url: null,
+    id: 0,
+  },
+  1: { title: "?????", order: 2, completed: false, tags: [], url: null, id: 1 },
+  2: {
+    title: "profit!",
+    order: 3,
+    completed: false,
+    tags: [],
+    url: null,
+    id: 2,
+  },
 };
-let nextId = 3;
 
-router.get('/todos/', list)
-  .del('/todos/', clear)
-  .post('/todos/', add)
-  .get('todo', '/todos/:id', show)
-  .patch('/todos/:id', update)
-  .del('/todos/:id', remove);
+let tags = {
+  0: { title: "Urgent", url: null },
+  1: { title: "Home", url: null },
+  2: { title: "Work", url: null },
+};
 
-async function list(ctx) {
-  ctx.body = Object.keys(todos).map(k => {
+let nextTodoId = 3;
+let nextTagId = 3;
+//TODOS
+
+router
+  .get("/todos/", listTodos)
+  .del("/todos/", clearTodos)
+  .post("/todos/", addTodos)
+  .get("todo", "/todos/:id", showTodos)
+  .patch("/todos/:id", updateTodos)
+  .del("/todos/:id", removeTodos)
+  .get("/todos/:id/tags", getTodoTags)
+  .post("/todos/:id/tags", addTodoTags)
+  .del("/todos/:id/tags", delTodoTags)
+  .del("/todos/:id/tags/:tag_id", delTodoTag);
+
+async function delTodoTag(ctx) {
+  const id = ctx.params.id;
+  const tag_id = ctx.params.tag_id;
+  const tagIndex = todos[id].tags.findIndex((tag) => tag.id == tag_id);
+  if (tagIndex > -1) {
+    todos[id].tags.splice(tagIndex, 1);
+  } else {
+    ctx.throw(404, { error: "Tag not found in todo" });
+  }
+  ctx.status = 204;
+}
+
+async function delTodoTags(ctx) {
+  const id = ctx.params.id;
+  todos[id].tags = [];
+  ctx.status = 204;
+}
+
+async function addTodoTags(ctx) {
+  const id = ctx.params.id;
+  const tag_id = ctx.request.body.id;
+  const tag = tags[tag_id];
+  if (!tag) {
+    console.log("tag not found");
+    ctx.throw(404, { error: "Tag not found" });
+  }
+  todos[id].tags.push(tag);
+  ctx.status = 201;
+  ctx.body = tag;
+}
+
+async function getTodoTags(ctx) {
+  const id = ctx.params.id;
+  const todo = todos[id];
+  if (!todo) ctx.throw(404, { error: "Todo not found" });
+  ctx.body = todo.tags;
+  ctx.status = 200;
+}
+
+async function listTodos(ctx) {
+  ctx.body = Object.keys(todos).map((k) => {
     todos[k].id = k;
     return todos[k];
   });
 }
 
-async function clear(ctx) {
+async function clearTodos(ctx) {
   todos = {};
   ctx.status = 204;
 }
 
-async function add(ctx) {
+async function addTodos(ctx) {
   const todo = ctx.request.body;
-  if (!todo.title) ctx.throw(400, {'error': '"title" is a required field'});
+  if (!todo.title) ctx.throw(400, { error: '"title" is a required field' });
   const title = todo.title;
-  if (!typeof data === 'string' || !title.length) ctx.throw(400, {'error': '"title" must be a string with at least one character'});
+  if (!typeof data === "string" || !title.length)
+    ctx.throw(400, {
+      error: '"title" must be a string with at least one character',
+    });
 
-  todo['completed'] = todo['completed'] || false;
-  todo['url'] = 'http://' + ctx.host + router.url('todo', nextId);
-  todos[nextId++] = todo;
+  todo["completed"] = todo["completed"] || false;
+  todo["url"] = "http://" + ctx.host + router.url("todo", nextTodoId);
+  todo["tags"] = [];
+  todo["id"] = nextTodoId;
+  todos[nextTodoId++] = todo;
 
   ctx.status = 303;
-  ctx.set('Location', todo['url']);
+  ctx.set("Location", todo["url"]);
 }
 
-async function show(ctx) {
+async function showTodos(ctx) {
   const id = ctx.params.id;
-  const todo = todos[id]
-  if (!todo) ctx.throw(404, {'error': 'Todo not found'});
+  const todo = todos[id];
+  if (!todo) ctx.throw(404, { error: "Todo not found" });
   todo.id = id;
   ctx.body = todo;
 }
 
-async function update(ctx) {
+async function updateTodos(ctx) {
   const id = ctx.params.id;
   const todo = todos[id];
 
@@ -63,11 +135,92 @@ async function update(ctx) {
   ctx.body = todo;
 }
 
-async function remove(ctx) {
+async function removeTodos(ctx) {
   const id = ctx.params.id;
-  if (!todos[id]) ctx.throw(404, {'error': 'Todo not found'});
+  if (!todos[id]) ctx.throw(404, { error: "Todo not found" });
 
   delete todos[id];
+
+  ctx.status = 204;
+}
+
+//TAGS
+
+router
+  .get("/tags/", listTags)
+  .del("/tags/", clearTags)
+  .post("/tags/", addTags)
+  .get("tag", "/tags/:id", showTags)
+  .patch("/tags/:id", updateTags)
+  .del("/tags/:id", removeTags)
+  .get("/tags/:id/todos", listTodosForTag);
+
+async function listTodosForTag(ctx) {
+  const tag_id = ctx.params.id;
+
+  let filtered_todos = Object.values(todos).filter((todo) => {
+    return todo.tags.some((tag) => tag.id == tag_id);
+  });
+
+  ctx.body = filtered_todos;
+}
+
+async function updateTags(ctx) {
+  const id = ctx.params.id;
+  const tag = tags[id];
+
+  Object.assign(tag, ctx.request.body);
+
+  ctx.body = tag;
+}
+
+async function listTags(ctx) {
+  ctx.body = Object.keys(tags).map((k) => {
+    tags[k].id = k;
+    return tags[k];
+  });
+}
+async function clearTags(ctx) {
+  tags = {};
+  ctx.status = 204;
+}
+async function addTags(ctx) {
+  const tag = ctx.request.body;
+  if (!tag.title) ctx.throw(400, { error: '"title" is a required field' });
+  const title = tag.title;
+  if (!typeof data === "string" || !title.length)
+    ctx.throw(400, {
+      error: '"title" must be a string with at least one character',
+    });
+  tag.id = nextTagId;
+  tag["url"] = "http://" + ctx.host + router.url("tag", nextTagId);
+  tag["todos"] = [];
+  tags[nextTagId++] = tag;
+  ctx.body = tag;
+  ctx.status = 201;
+  ctx.set("Location", tag["url"]);
+}
+
+async function showTags(ctx) {
+  const id = Number(ctx.params.id);
+
+  const tag = tags[id];
+
+  if (!tag) ctx.throw(404, { error: "Tag not found" });
+  tag.id = id;
+  ctx.body = tag;
+}
+async function updateTags(ctx) {
+  const id = ctx.params.id;
+  const tag = tags[id];
+  Object.assign(tag, ctx.request.body);
+  ctx.body = tag;
+}
+async function removeTags(ctx) {
+  const id = ctx.params.id;
+  if (!tags[id]) ctx.throw(404, { error: "Todo not found" });
+
+  delete tags[id];
 
   ctx.status = 204;
 }
